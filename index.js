13711345b0253a2f33c6cfa028670f4cd2f2338a
krisskash <Papafragos24@gmail.com>
2025-04-11 14:41:23 +0300

    Change Appearance

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 // For more info see docs.battlesnake.com
 
 import runServer from './server.js';
+import chalk from 'chalk';
 
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
@@ -20,10 +21,10 @@ function info() {
 
   return {
     apiversion: "1",
-    author: "",       // TODO: Your Battlesnake Username
-    color: "#888888", // TODO: Choose color
-    head: "default",  // TODO: Choose head
-    tail: "default",  // TODO: Choose tail
+    author: "B1G_THAN0S, L1L 4GGELOS, CHR1S SL1M3",
+    color: "#D2042D", // Cherry Red
+    head: "silly",    // Silly face head
+    tail: "bolt",     // Lightning bolt tail
   };
 }
 
@@ -40,7 +41,52 @@ function end(gameState) {
 // move is called on every turn and returns your next move
 // Valid moves are "up", "down", "left", or "right"
 // See https://docs.battlesnake.com/api/example-move for available data
+function printBoard(gameState) {
+  const board = gameState.board;
+  const width = board.width;
+  const height = board.height;
+
+  // Create empty board
+  let boardMap = new Map();
+
+  // Mark food with apple emoji
+  board.food.forEach(f => {
+    boardMap.set(`${f.x},${f.y}`, '🍎');
+  });
+
+  // Mark snakes - body with snake emoji, head with crown
+  board.snakes.forEach(snake => {
+    const isMySnake = snake.id === gameState.you.id;
+    snake.body.forEach((b, i) => {
+      if (i === 0) {
+        // Snake head
+        boardMap.set(`${b.x},${b.y}`, isMySnake ?
+          chalk.green('👑') :
+          chalk.red('👑'));
+      } else {
+        // Snake body
+        boardMap.set(`${b.x},${b.y}`, isMySnake ?
+          chalk.green('🟩') :
+          chalk.red('🟥'));
+      }
+    });
+  });
+
+  // Print the board
+  for (let y = height - 1; y >= 0; y--) {
+    for (let x = 0; x < width; x++) {
+      const cell = boardMap.get(`${x},${y}`) || '⬜';
+      process.stdout.write(cell + ' ');
+    }
+    console.log(); // New line after each row
+  }
+  console.log(); // Empty line after board
+}
+
 function move(gameState) {
+  // Print the game state and board
+  console.log(`\nTurn ${gameState.turn}`);
+  printBoard(gameState);
 
   let isMoveSafe = {
     up: true,
@@ -66,15 +112,58 @@ function move(gameState) {
     isMoveSafe.up = false;
   }
 
-  // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-  // boardWidth = gameState.board.width;
-  // boardHeight = gameState.board.height;
+  // Step 1 - Prevent your Battlesnake from moving out of bounds
+  const boardWidth = gameState.board.width;
+  const boardHeight = gameState.board.height;
 
-  // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-  // myBody = gameState.you.body;
+  if (myHead.x + 1 >= boardWidth) {
+    isMoveSafe.right = false;
+  }
+  if (myHead.x - 1 < 0) {
+    isMoveSafe.left = false;
+  }
+  if (myHead.y + 1 >= boardHeight) {
+    isMoveSafe.up = false;
+  }
+  if (myHead.y - 1 < 0) {
+    isMoveSafe.down = false;
+  }
 
-  // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-  // opponents = gameState.board.snakes;
+  // Step 2 - Prevent your Battlesnake from colliding with itself
+  const myBody = gameState.you.body;
+  myBody.forEach(segment => {
+    if (segment.x === myHead.x + 1 && segment.y === myHead.y) {
+      isMoveSafe.right = false;
+    }
+    if (segment.x === myHead.x - 1 && segment.y === myHead.y) {
+      isMoveSafe.left = false;
+    }
+    if (segment.x === myHead.x && segment.y === myHead.y + 1) {
+      isMoveSafe.up = false;
+    }
+    if (segment.x === myHead.x && segment.y === myHead.y - 1) {
+      isMoveSafe.down = false;
+    }
+  });
+
+  // Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
+  const opponents = gameState.board.snakes;
+  opponents.forEach(snake => {
+    snake.body.forEach(segment => {
+      if (segment.x === myHead.x + 1 && segment.y === myHead.y) {
+        isMoveSafe.right = false;
+      }
+      if (segment.x === myHead.x - 1 && segment.y === myHead.y) {
+        isMoveSafe.left = false;
+      }
+      if (segment.x === myHead.x && segment.y === myHead.y + 1) {
+        isMoveSafe.up = false;
+      }
+      if (segment.x === myHead.x && segment.y === myHead.y - 1) {
+        isMoveSafe.down = false;
+      }
+    });
+  });
 
   // Are there any safe moves left?
   const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
