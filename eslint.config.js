58848d8f24fa61a3a553f338e300af50430a6be8
thanos <thanoslivaditis@gmail.com>
2025-05-08 22:52:26 +0300

    Fix ESLint issues

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslintComments from 'eslint-plugin-eslint-comments';
+import sonarjs from 'eslint-plugin-sonarjs';
+import unicorn from 'eslint-plugin-unicorn';
+
+export default [
+  {
+    files: ['**/*.js'],
+    plugins: {
+      'eslint-comments': eslintComments,
+      'sonarjs': sonarjs,
+      'unicorn': unicorn,
+    },
+    rules: {
+      'eslint-comments/no-unlimited-disable': 'warn',
+      'sonarjs/no-duplicate-string': 'warn',
+      'unicorn/no-abusive-eslint-disable': 'error',
+    },
+  },
+];
