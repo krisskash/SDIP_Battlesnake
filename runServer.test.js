87cb63bb4f5900aee767cee60ad79fb597809e38
thanosleiv <157423947+thanosleiv@users.noreply.github.com>
2025-05-22 18:23:08 +0300

    Create runServer.test.js

---
+++ runServer.test.js
@@ -0,0 +1,14 @@
+import { runServer } from './server.js';
+
+describe('runServer()', () => {
+  it('should call the server setup function without errors', () => {
+    const handlers = {
+      info: jest.fn(),
+      start: jest.fn(),
+      move: jest.fn(),
+      end: jest.fn(),
+    };
+
+    expect(() => runServer(handlers)).not.toThrow();
+  });
+});
