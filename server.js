2008502f1d60772fdce3c7c17ac70b0c32c902a7
thanosleiv <157423947+thanosleiv@users.noreply.github.com>
2025-05-22 17:49:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import express from 'express';
-
 export function move(gameState) {
   const myHead = gameState.you.body[0];
   const food = gameState.board.food;
@@ -60,39 +58,4 @@ export function move(gameState) {
       : 'down';
 
   return { move: nextMove };
-}
-
-export default function runServer(handlers) {
-  const app = express();
-  app.use(express.json());
-
-  app.get('/', (req, res) => {
-    res.send(handlers.info());
-  });
-
-  app.post('/start', (req, res) => {
-    handlers.start(req.body);
-    res.send('ok');
-  });
-
-  app.post('/move', (req, res) => {
-    res.send(handlers.move(req.body));
-  });
-
-  app.post('/end', (req, res) => {
-    handlers.end(req.body);
-    res.send('ok');
-  });
-
-  app.use(function (req, res, next) {
-    res.set('Server', 'battlesnake/github/starter-snake-javascript');
-    next();
-  });
-
-  const host = '0.0.0.0';
-  const port = process.env.PORT || 8000;
-
-  app.listen(port, host, () => {
-    console.log(`Running Battlesnake at http://${host}:${port}...`);
-  });
 }
