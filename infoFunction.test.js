b5a8cede94a6f623779306ab616d4760e8d75efb
thanosleiv <157423947+thanosleiv@users.noreply.github.com>
2025-05-22 18:56:14 +0300

    Create infoFunction.test.js

---
+++ infoFunction.test.js
@@ -0,0 +1,10 @@
+import { info } from './server.js';
+
+describe('info()', () => {
+  it('should return the correct Battlesnake info', () => {
+    const result = info();
+    expect(result).toHaveProperty('apiversion', '1');
+    expect(result).toHaveProperty('author');
+    expect(result.author).toBe('B1G_THAN0S, L1L 4GGELOS, CHR1S SL1M3');
+  });
+});
